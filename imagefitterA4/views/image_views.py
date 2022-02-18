0b8bb88782e950a8ff91a3dd8a660f7a80bc0214
jullieth705 <jullieth9111@gmail.com>
2022-02-18 11:27:12 -0500

    Refactor views files

--- imagefitterA4/views/image_views.py
+++ imagefitterA4/views/image_views.py
@@ -1,65 +1,17 @@
 from django.shortcuts import  render, redirect
-from .forms import NewUserForm, ImageForm, LoginUserForm
-from django.contrib.auth import login, authenticate, logout
+from ..forms import ImageForm
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-from .models import ImageResource
+from ..models import ImageResource
 from PIL import Image
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_safe, require_http_methods
-
-HOME_URL = 'imagefitterA4:home' 
-
-@require_safe
-@login_required
-def home(request):
-	return render(request=request, template_name='./home.html') 
 
 @require_safe
 @login_required
 def galery(request):
 	images = ImageResource.objects.filter(user=request.user)
 	return render(request=request, template_name='./galery.html', context={'images':images}) 
-
-@require_http_methods(['GET', 'POST'])
-def register_request(request):
-	if request.method == 'POST':
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, 'Registro exitoso.' )
-			return redirect(HOME_URL)
-		messages.error(request, 'Registro fallido.')
-	form = NewUserForm()
-	return render (request=request, template_name='./register.html', context={'register_form':form})
-
-@require_http_methods(['GET', 'POST'])
-def login_request(request):
-	if request.method == 'POST':
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.info(request, f'Has iniciado sesión como {username}.')
-				URL = request.GET['next'] if 'next' in request.GET else HOME_URL
-				return redirect(URL)
-			else:
-				messages.error(request,'Usuario o contraseña incorrectos.')
-		else:
-			messages.error(request,'Usuario o contraseña incorrectos.')
-	form = LoginUserForm()
-	return render(request=request, template_name='./login.html', context={'login_form':form})
-
-@require_safe
-def logout_request(request):
-	logout(request)
-	messages.info(request, 'Has cerrado sesión exitosamente.') 
-	return redirect('imagefitterA4:login')
 
 def fit_image(source, name, number_file):
 	image = Image.open(source)
